e729602144712743ac6206f248a18ef83c4c84d0
modifyingk <116068100+modifyingk@users.noreply.github.com>
2023-08-05 23:14:21 +0900

    Add files via upload

---
+++ search_algorithm/BinSearchTester.java
@@ -0,0 +1,57 @@
+package search_algorithm;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinSearchTester {
+
+	static int binSearch(int[] a, int n, int key) {
+		int pl = 0;
+		int pr = n - 1;
+		
+		do {
+			int pc = (pl + pr) / 2;
+			if(a[pc] == key) {
+				return pc;
+			} else if(a[pc] < key) {
+				pl = pc + 1;
+			} else {
+				pr = pc - 1;
+			}
+		} while(pl <= pr);
+		
+		return -1;
+		
+	}
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("요솟수: ");
+		int num = sc.nextInt();
+		
+		int[] x = new int[num];
+		
+		System.out.println("오른차순으로 입력하세요.");
+		System.out.print("x[0]: ");
+		x[0] = sc.nextInt();
+		
+		for(int i = 1; i < num; i++) {
+			do {
+				System.out.print("x[" + i + "]: ");
+				x[i] = sc.nextInt();
+			} while(x[i] < x[i - 1]);
+		}
+		
+		System.out.print("검색할 값: ");
+		int ky = sc.nextInt();
+		
+		int idx = Arrays.binarySearch(x, ky);
+		
+		if(idx < 0) {
+			System.out.println("그 값의 요소가 없습니다.");
+		} else {
+			System.out.println("그 값은 x[" + idx + "]에 있습니다.");
+		}
+	}
+
+}
