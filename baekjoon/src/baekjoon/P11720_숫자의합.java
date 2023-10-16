6901bfb2d6b17fb423e29e1703f15d7b8ec199e6
modifyingk <116068100+modifyingk@users.noreply.github.com>
2023-10-16 19:37:04 +0900

    Add files via upload

---
+++ baekjoon/src/baekjoon/P11720_숫자의합.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class P11720_숫자의합 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		String sNum = sc.next();
+		char[] cNum = sNum.toCharArray();
+		
+		int sum = 0;
+		for(int i = 0; i < cNum.length; i++) {
+			sum += cNum[i] - 48;
+		}
+		System.out.println(sum);
+	}
+
+}
