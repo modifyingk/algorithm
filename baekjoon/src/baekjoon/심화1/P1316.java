eba651c9dca3663439e4720ca5f460195ebd5786
modifyingk <116068100+modifyingk@users.noreply.github.com>
2024-02-26 21:33:54 +0900

    Update P1316.java

--- baekjoon/src/baekjoon/심화1/P1316.java
+++ baekjoon/src/baekjoon/심화1/P1316.java
@@ -8,8 +8,32 @@ public class P1316 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
+		int count = 0;
 		for(int i = 0; i < n; i++) {
-			
+			String s = br.readLine();
+			int[] num = new int[26];
+			for(int j = 0; j < s.length(); j++) {
+				num[s.charAt(j) - 'a']++;
+			}
+			boolean chk = false;
+			for(int j = 0; j < 26; j++) {
+				String g = "";
+				if(num[j] <= 1)
+					chk = true;
+				else {
+					for(int k = 0; k < num[j]; k++) {
+						g += (char)(j + 'a');
+					}
+					if(s.contains(g))
+						chk = true;
+					else {
+						chk = false;
+						break;
+					}
+				}
+			}
+			if(chk) count++;
 		}
+		System.out.println(count);
 	}
 }
