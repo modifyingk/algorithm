42eae1551fc33b64322ecbeb1b856bc2769f3a20
modifyingk <116068100+modifyingk@users.noreply.github.com>
2024-03-05 22:28:58 +0900

    Add files via upload

---
+++ baekjoon/src/baekjoon/기하_직사각형과삼각형/P27323.java
@@ -0,0 +1,14 @@
+package baekjoon.기하_직사각형과삼각형;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P27323 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		System.out.println(a * b);
+	}
+}
