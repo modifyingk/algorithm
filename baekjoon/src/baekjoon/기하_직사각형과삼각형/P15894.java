84c0346d2c4896eda3bf2db64cd514be193a3c4d
modifyingk <116068100+modifyingk@users.noreply.github.com>
2024-03-11 10:49:42 +0900

    Add files via upload

---
+++ baekjoon/src/baekjoon/기하_직사각형과삼각형/P15894.java
@@ -0,0 +1,12 @@
+package baekjoon.기하_직사각형과삼각형;
+
+import java.util.Scanner;
+
+public class P15894 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		System.out.println(4 * n);
+		sc.close();
+	}
+}
