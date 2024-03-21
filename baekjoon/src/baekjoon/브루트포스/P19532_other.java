e49910888ecd58eac644fad2501819346ce1f1a8
modifyingk <116068100+modifyingk@users.noreply.github.com>
2024-03-21 14:57:09 +0900

    Add files via upload

---
+++ baekjoon/src/baekjoon/브루트포스/P19532_other.java
@@ -0,0 +1,23 @@
+package baekjoon.브루트포스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P19532_other {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		int f = Integer.parseInt(st.nextToken());
+		
+		int x = (c * e - f * b) / (a * e - d * b);
+		int y = (c * d - f * a) / (b * d - e * a);
+		System.out.println(x + " " + y);
+	}
+}
