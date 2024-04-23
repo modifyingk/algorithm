b2c6e97e428b2604c852404fe7193288a03e4e34
Sujung Kim <116068100+modifyingk@users.noreply.github.com>
2024-04-23 16:49:32 +0900

    Add files via upload

---
+++ baekjoon/src/baekjoon/스택_큐_덱/P10773.java
@@ -0,0 +1,26 @@
+package baekjoon.스택_큐_덱;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class P10773 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int k = Integer.parseInt(br.readLine());
+		Stack<Integer> stack = new Stack<>();
+		for(int i = 0; i < k; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if(num == 0)
+				stack.pop();
+			else
+				stack.push(num);
+		}
+		int sum = 0;
+		while(!stack.isEmpty()) {
+			sum += stack.pop();
+		}
+		System.out.println(sum);
+	}
+}
