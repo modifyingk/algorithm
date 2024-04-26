140d5a2514355afa382637eaa737616f42d7d11b
Sujung Kim <116068100+modifyingk@users.noreply.github.com>
2024-04-26 20:50:00 +0900

    Add files via upload

---
+++ baekjoon/src/baekjoon/약수배수소수2/P13909.java
@@ -0,0 +1,15 @@
+package baekjoon.약수배수소수2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P13909 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		System.out.println((int)Math.sqrt(n));
+	}
+
+}
