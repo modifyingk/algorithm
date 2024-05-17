19c2c4ac78433158bc70c3a033e22d9925a4ada2
Sujung Kim <116068100+modifyingk@users.noreply.github.com>
2024-05-17 22:10:21 +0900

    Update P11729.java

--- baekjoon/src/baekjoon/재귀함수/P11729.java
+++ baekjoon/src/baekjoon/재귀함수/P11729.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class P11729 {
 
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
 	static int count = 0;
 	
 	public static void main(String[] args) throws IOException {
@@ -12,6 +13,8 @@ public class P11729 {
 		
 		int n = Integer.parseInt(br.readLine());
 		hanoi(n, 1, 3, 2);
+		bw.append(count + "\n");
+		bw.append(sb);
 		bw.flush();
 		bw.close();
 	}
@@ -19,11 +22,11 @@ public class P11729 {
 	private static void hanoi(int n, int from, int to, int tmp) throws IOException {
 		if (n == 1) {
 			count++;
-			bw.append(from + " " + to + "\n");
+			sb.append(from + " " + to + "\n");
 			return;
 		} else {
 			hanoi(n - 1, from, tmp, to);
-			bw.append(from + " " + to + "\n");
+			sb.append(from + " " + to + "\n");
 			count++;
 			hanoi(n - 1, tmp, to, from);
 		}
